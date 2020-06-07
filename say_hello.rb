ef1912c5431ec35808ffcfc6a66f8cc35123a22a
Natalia Galan <n8galan@gmail.com>
2020-06-07 03:30:39 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello(first_name = "Ruby", last_name = "Programmer!")
-  puts "Hello #{first_name, last_name!}"
+def say_hello[first_name = "Ruby" last_name = "Programmer!"]
+  puts "Hello #{first_name last_name!}"
 end
 
-say_hello(Natalia, Galan)
+say_hello(Natalia Galan)
